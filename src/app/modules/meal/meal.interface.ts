c7794546e637f27df86ccb7c16ce1eacd9dd2ef9
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-10-19 11:54:09 +0600

    Add: meal interface and model

---
+++ src/app/modules/meal/meal.interface.ts
@@ -0,0 +1,7 @@
+export interface IMeal {
+  name: string
+  type: 'break_fast' | 'lunch' | 'dinner'
+  price: number
+  image: string
+  description: string
+}
---
+++ src/app/modules/meal/meal.model.ts
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose'
+import { IMeal } from './meal.interface'
+
+const mealSchema = new Schema<IMeal>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+export const Meal = model<IMeal>('Meal', mealSchema)
