de935d2634a665bddac5bde10c2eff452df00883
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2025-01-15 22:55:35 +0600

    Remove reason from withdraw

--- src/app/modules/withdraw/withdraw.validation.ts
+++ src/app/modules/withdraw/withdraw.validation.ts
@@ -6,19 +6,6 @@ const createWithdrawReqValidationSchema = Joi.object({
   }),
 })
 
-// const cancelReqStatusValidationSchema = Joi.object({
-//   body: Joi.object({
-//     status: Joi.string()
-//       .valid('approved', 'canceled', 'pending')
-//       .required()
-//       .messages({
-//         'any.only': 'Status must be approved or canceled or pending',
-//         'any.required': 'Status is required',
-//       }),
-//   }),
-// })
-
 export const withdrawValidations = {
   createWithdrawReqValidationSchema,
-  // cancelReqStatusValidationSchema,
 }
